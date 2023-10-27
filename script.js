57f51450db316271d53099be5a47b2dce470e2d1
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2023-10-27 23:33:19 +0100

    layered while loop added that adds a new character onto the password until complete

--- script.js
+++ script.js
@@ -13,24 +13,15 @@ var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
-//* Generate a password when the button is clicked
-
-  //* Code should validate for each input and at least one character type should be selected
-  //* Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-
-// specialCharacters
-// numericCharacters
-// upperCasedCharacters
-// lowerCasedCharacters
   
 // Function to prompt user for password options
 function getPasswordOptions() {
 
   var length = 0;
-
+  // ask for a password length until a correct answer has been recieved 
   while (isNaN(length) || length < 8 || length > 128){
     length = Number(prompt("Pick a password length between 8 - 128"));
-
+    // handle error
     if (isNaN(length)){
       alert("Please enter a number only.");
     }
@@ -41,7 +32,7 @@ function getPasswordOptions() {
   var uppercase = false;
   var numerical = false;
   var special = false;
-// if no setting have been selected then prompt the user for more their entry.
+// if no setting have been selected then prompt the user for them to accept at least one entry.
   while (lowercase===false && uppercase===false && numerical===false && special===false){
     alert("Please choose which settings you would like in your password.")
     lowercase = confirm("Would you like to include Lowercase Characters?");
@@ -96,15 +87,21 @@ function generatePassword() {
   }).map(function(setting) {
     return setting.arrayChar; //extracts the arrays of characters from the objects that have been chosen for the password.
   });
-  console.log(arrays);
+  // console.log(arrays);
+
+
   // generate the random characters for the password to the length of password chosen
   var password = "";
+  var i = -1;
   while (password.length < chosenLength){
-    var randomIndex = Math.floor(Math.random() * (arrays.length));
-    console.log("Random index: " + randomIndex);
-    password = password + getRandom(arrays[randomIndex]);
-    console.log("Password so far: " + password)
+    i = 0 // start from the first index in arrays, when every array has been accessed then restart from 0
+    while(i < arrays.length){ // get a random character from the next array in the arrays until one character has been recieved out of each array or the password has reached its total length
+      password = password + getRandom(arrays[i]) // add the character to the password
+      console.log("updated password")
+      i++
+    }
   }
+
 
   return password
 }
